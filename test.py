a43d4577987e764bbcd310f8582ef5ba5e08f2a8
tookubo <annexdev02ookubo@gmail.com>
2015-11-16 15:37:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
+from hoge import Hoge
+import pytest
+
+class TestHoge1(object):
+
+    def pytest_funcarg__hoge(request):
+        return Hoge(1)
+
+    def test_type(self, hoge):
+        assert isinstance(hoge, Hoge)
+
+    def test_val(self, hoge):
+        assert hoge.val == 1
+
+        hoge.update('hoge')
+        assert hoge.val == 'hoge'
+
+if __name__ == '__main__':
+    pytest.main()
 
 print "hello"
